50ceedbf7283509f77056e322bffdbde5f4ff381
Lee Chun On <jeffreyleeon@gmail.com>
2016-07-31 12:16:39 +0800

    Attaching dummy HandListener to HandCycler prefabs

---
+++ Assets/Scripts/HandListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandListener : MonoBehaviour {
+
+	private HandController handController;
+
+	// Use this for initialization
+	void Start () {
+		handController = GetComponent<HandController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateHandStore (); // Updating graphical hands
+	}
+
+	private void UpdateHandStore () {
+		HandModel[] handModelArray = handController.GetAllGraphicsHands ();
+		// Temp
+//		Debug.Log ("====Hand model array: " + handModelArray.Length);
+//		for (int i = 0; i < handModelArray.Length; i++) {
+//			HandModel handModel = handModelArray [i];
+//			Leap.Hand hand = handModel.GetLeapHand ();
+//			Debug.Log ("======hand is left? " + hand.IsLeft);
+//			Debug.Log ("======hand is right? " + hand.IsRight);
+//		}
+	}
+}
